4083c3e6b3b6e12b47323d4e82ab1b02fa69e040
Mehmet Ege Sarıgöl <mehmetegesarigol@gmail.com>
2026-04-21 13:14:03 +0300

    Update priority_queue.h

--- project_folder/priority_queue.h
+++ project_folder/priority_queue.h
@@ -1,1 +1,22 @@
+#ifndef PRIORITY_QUEUE_H
+#define PRIORITY_QUEUE_H
 
+#include "rfc_parser.h"
+
+// Min-Heap yapısının kalbi
+typedef struct {
+    HttpPriority* data; // Dinamik dizi pointer'ı
+    int capacity;       // Toplam kapasite
+    int size;           // Mevcut eleman sayısı
+} MinHeap;
+
+// Bellek Yönetimi ve Temel Fonksiyon Prototipleri
+MinHeap* create_heap(int initial_capacity);
+void free_heap(MinHeap* heap);
+
+// O(log n) karmaşıklığına sahip Kuyruk İşlemleri
+void insert_request(MinHeap* heap, HttpPriority request);
+HttpPriority extract_min(MinHeap* heap);
+bool is_empty(MinHeap* heap);
+
+#endif // PRIORITY_QUEUE_H
